cf8f5354ad6d78c319c0fea2f36719441ebf4920
mshqlaih <60563969+mshqlaih@users.noreply.github.com>
2026-04-26 18:05:25 +0200

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,13 +1,31 @@
 'use strict';
 
-const CACHE_NAME = 'qmc-app-cache-v1.13';
+const CACHE_NAME = 'qmc-app-cache-v1.14';
 
 const CORE_ASSETS = [
   './',
   './index.html',
   './main.dart.js',
   './flutter.js',
+  './flutter_bootstrap.js',
+  './favicon.png',
+  './manifest.json',
+  './version.json',
+  './.last_build_id',
+  './assets/AssetManifest.json',
+  './assets/FontManifest.json',
+  './assets/NOTICES',
+  './icons/Icon-192.png',
+  './icons/Icon-512.png',
+  './icons/Icon-maskable-192.png',
+  './icons/Icon-maskable-512.png',
+  './canvaskit/canvaskit.js',
+  './canvaskit/canvaskit.wasm',
+  './canvaskit/chromium/canvaskit.js',
+  './canvaskit/chromium/canvaskit.wasm',
+  './assets/assets/fonts/Tajawal-Regular.ttf',
 ];
+
 
 
 self.addEventListener('install', (event) => {
