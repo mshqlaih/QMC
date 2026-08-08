f035dbacda780245ceb6bfb02d00344a1cddcae3
mshqlaih <mshqlaih@iugaza.edu.ps>
2026-08-08 15:08:17 +0200

    1.1.9+16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1088298706" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3962201799" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
